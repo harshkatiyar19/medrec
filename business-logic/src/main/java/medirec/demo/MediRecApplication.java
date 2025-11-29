1ee5c537017990e1615ca9bb3d934370393919b0
harshkatiyar19 <122049188+harshkatiyar19@users.noreply.github.com>
2025-11-29 18:02:06 +0530

    initialization

---
+++ business-logic/src/main/java/medirec/demo/MediRecApplication.java
@@ -0,0 +1,13 @@
+package medirec.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediRecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediRecApplication.class, args);
+	}
+
+}
---
+++ business-logic/src/test/java/medirec/demo/MediRecApplicationTests.java
@@ -0,0 +1,13 @@
+package medirec.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediRecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
